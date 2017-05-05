7c4b230372c522b8ce0c94d9f2ce0dd14dd53eff
d1hacker <ddroid991@gmail.com>
2017-05-04 23:17:39 -0300

    Coloca para pegar pelo CEP do cara

--- app/src/main/java/chamaae/com/br/ActivityInfEnd.java
+++ app/src/main/java/chamaae/com/br/ActivityInfEnd.java
@@ -1,16 +1,31 @@
 package chamaae.com.br;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ActivityInfEnd extends AppCompatActivity {
 
     Spinner  SpUF;
-    EditText EdtCep,EdtCidade,EdtUF,EdtRua,EdtNro;
+    EditText EdtCep,EdtCidade,EdtBairro,EdtRua,EdtNro;
     Button   BtnBuscaCEP,BtnBuscaEND,BtnSalvar;
 
     String EstadosBR[] = {" ","AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"};
@@ -20,10 +35,122 @@ public class ActivityInfEnd extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inf_end);
 
-        SpUF = (Spinner) findViewById(R.id.SpUf);
+        SpUF         = (Spinner)  findViewById(R.id.SpUf);
+        BtnBuscaCEP  = (Button)   findViewById(R.id.BtnBuscarCEP);
+        EdtCep       = (EditText) findViewById(R.id.EdtCep);
 
         ArrayAdapter AdaptadorSpUF = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,EstadosBR);
         SpUF.setAdapter(AdaptadorSpUF);
 
+        BtnBuscaCEP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PegaCEP pegaCEP = new PegaCEP();
+                if(EdtCep.getText().toString().isEmpty()){
+                 Toast.makeText(getApplicationContext(),"INFORME O CEP !",Toast.LENGTH_LONG).show();
+                }else {
+                    pegaCEP.execute("https://viacep.com.br/ws/" + EdtCep.getText().toString() + "/json/");
+                }
+            }
+        });
+
+
     }
+
+    public class PegaCEP extends AsyncTask<String, Void, String> {
+        String data;
+        String Cidade,Rua,Bairro,UF;
+        ProgressDialog pg = new ProgressDialog(ActivityInfEnd.this);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pg.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            pg.setMessage("CARREGANDO DADOS AGUARDE ...");
+            pg.setCancelable(false);
+            pg.show();
+        }
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                TrataJson(PegaJson(strings[0]));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            EdtCidade = (EditText) findViewById(R.id.EdtCidade);
+            EdtRua    = (EditText) findViewById(R.id.EdtRua);
+            EdtBairro = (EditText) findViewById(R.id.EdtBairro);
+
+            EdtBairro.setText(Bairro);
+            EdtRua.setText(Rua);
+            EdtCidade.setText(Cidade);
+            for(int i=0; i<EstadosBR.length;i++){
+                if(EstadosBR[i].equals(UF)){
+                    Log.i("UF",EstadosBR[i]);
+                    SpUF.setSelection(i);
+                }
+            }
+
+            pg.dismiss();
+
+        }
+        private String PegaJson(String lnk) throws IOException {
+            data = "";
+            InputStream iStream = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                URL url = new URL(lnk);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+                iStream = urlConnection.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+                StringBuffer sb = new StringBuffer();
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+                data = sb.toString();
+                br.close();
+            } catch (Exception e) {
+                Log.d("Error while downloading", e.toString());
+            } finally {
+                iStream.close();
+                urlConnection.disconnect();
+            }
+
+            Log.i("JSON",data);
+            return data;
+
+        }
+        private void TrataJson(String json){
+
+            try {
+                JSONObject jsonObject = new JSONObject(json);
+
+                Cidade = jsonObject.getString("localidade");
+                Rua    = jsonObject.getString("logradouro");
+                Bairro = jsonObject.getString("bairro");
+                UF     = jsonObject.getString("uf");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+
+    }
+
+
+
+
+
+
 }
